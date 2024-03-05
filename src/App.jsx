8120f95b4757536d1d5bc441ad34fbf140fa4c1d
Rafael <rafamed.tech@gmail.com>
2024-03-04 18:30:46 -0800

    Refactor App component and menu styles

--- src/App.jsx
+++ src/App.jsx
@@ -10,25 +10,36 @@ function App() {
 
       <main>
         <div className='container mx-auto p-4 flex flex-col gap-2'>
-          <div>
+          <section>
             <h2 className='text-3xl text-primary'>Menú de la semana</h2>
             <p>Correspondiente a la semana del 4 al 8 de Marzo</p>
-          </div>
+          </section>
 
-          <div className='h-[35rem] mt-8 carousel carousel-horizontal carousel-center gap-2 rounded-box mx-auto'>
+          <button className='btn w-fit btn-primary mx-auto'>Ordenar</button>
+
+          <section className='h-[35rem] mt-4 carousel carousel-horizontal carousel-center gap-2 rounded-box mx-auto'>
             {days.map(({ name, image }) => (
               <div className='carousel-item h-full' key={name} id={name}>
                 <img src={image} className='rounded-box h-full w-80' />
               </div>
             ))}
-          </div>
-          <div className='flex justify-center w-full py-2 gap-2'>
+          </section>
+
+          <section className='flex justify-center w-full py-2 gap-2 px-4'>
             {days.map(({ name }) => (
-              <a href={`#${name}`} key={name} className='btn btn-xs btn-primary'>
+              <a href={`#${name}`} key={name} className=' badge  badge-primary badge-lg'>
                 {name}
               </a>
             ))}
-          </div>
+          </section>
+
+          <section className='bg-neutral text-base-100 flex p-4 items-center gap-4 mt-4 rounded-box'>
+            <p className='text-primary text-2xl'>
+              ¿Tienes una dieta de tu nutriólogo? <br />{' '}
+              <span className='text-base-100 text-lg'>¡La cocinamos a la medida!</span>
+            </p>
+            <button className='btn btn-primary'>¡Cotizar!</button>
+          </section>
         </div>
       </main>
     </>
--- src/data/menu.js
+++ src/data/menu.js
@@ -1,26 +1,26 @@
 export const days = [
   {
-    name: 'Lunes',
+    name: 'Lun',
     image:
       'https://res.cloudinary.com/rafamed-dev/image/upload/v1708636073/heltifud/26%20Feb%20-%201%20Mar/Lunes_ium3ke.png',
   },
   {
-    name: 'Martes',
+    name: 'Mar',
     image:
       'https://res.cloudinary.com/rafamed-dev/image/upload/v1708636073/heltifud/26%20Feb%20-%201%20Mar/Martes_tfhkh0.png',
   },
   {
-    name: 'Miercoles',
+    name: 'Mie',
     image:
       'https://res.cloudinary.com/rafamed-dev/image/upload/v1708636073/heltifud/26%20Feb%20-%201%20Mar/Miercoles_wjb5ls.png',
   },
   {
-    name: 'Jueves',
+    name: 'Jue',
     image:
       'https://res.cloudinary.com/rafamed-dev/image/upload/v1708636073/heltifud/26%20Feb%20-%201%20Mar/Jueves_gfzwfx.png',
   },
   {
-    name: 'Viernes',
+    name: 'Vie',
     image:
       'https://res.cloudinary.com/rafamed-dev/image/upload/v1708636073/heltifud/26%20Feb%20-%201%20Mar/Viernes_fqvrmr.png',
   },
