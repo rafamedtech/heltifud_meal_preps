8ac97d2e0811c302449e4e11cc34a17442c9bf05
Rafael Amed Valenzuela <69322700+rafamedtech@users.noreply.github.com>
2025-05-15 18:45:20 -0700

    feat(nav): enhance MobileNav component with variant prop and whitespace adjustments

--- layers/base/app/components/MobileNav.vue
+++ layers/base/app/components/MobileNav.vue
@@ -1,11 +1,14 @@
+<script setup lang="ts"></script>
+
 <template>
   <UNavigationMenu
     orientation="horizontal"
+    variant="link"
     :items="links"
     :ui="{
       root: 'justify-between w-full [&>div]:min-w-full',
       item: 'flex-[1]',
-      link: 'flex-col',
+      link: 'flex-col ',
     }"
   />
 </template>
