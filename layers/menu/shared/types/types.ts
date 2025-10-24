ce41a3dcc03f516d061b3786507bca8d0298b538
Rafael <rafamed.tech@gmail.com>
2025-10-24 10:59:38 -0700

    fix(menu): update menu activation status and modify dish names for accuracy

--- layers/menu/shared/types/types.ts
+++ layers/menu/shared/types/types.ts
@@ -1,58 +1,58 @@
 export type WeeklyPlan = {
-  id: number;
-  title: string;
-  price: number;
-  description: string;
-  image: string;
+  id: number
+  title: string
+  price: number
+  description: string
+  image: string
   button: {
-    label: string;
-    icon: string;
-    click: () => void;
-  };
-};
+    label: string
+    icon: string
+    click: () => void
+  }
+}
 
 export interface FoodItem {
-  id: number;
-  name: string;
-  calories: number;
+  id: number
+  name: string
+  calories: number
 }
 
 export interface Meal {
-  id: number;
-  mainDish: FoodItem;
-  side1?: FoodItem | null;
-  side2?: FoodItem | null;
-  ramekin: FoodItem;
-  contenedor: string;
-  image: string;
-  description: string;
+  id: number
+  mainDish: FoodItem
+  side1?: FoodItem | null
+  side2?: FoodItem | null
+  ramekin: FoodItem | null
+  contenedor: string
+  image: string
+  description: string
 }
 
 export interface DayMenu {
-  id: number;
-  dayOfWeek: string;
-  breakfast: Meal;
-  lunch: Meal;
-  dinner: Meal;
-  snack1: Meal;
-  snack2: Meal;
+  id: number
+  dayOfWeek: string
+  breakfast: Meal
+  lunch: Meal
+  dinner: Meal
+  snack1: Meal
+  snack2: Meal
 }
 
 export interface WeeklyMenu {
-  id: number;
-  createdAt: string;
-  updatedAt: string;
-  startDate: string;
-  endDate: string;
-  name: string;
-  isActive: boolean;
-  daysStd: DayMenu[];
-  daysVeg: DayMenu[];
+  id: number
+  createdAt: string
+  updatedAt: string
+  startDate: string
+  endDate: string
+  name: string
+  isActive: boolean
+  daysStd: DayMenu[]
+  daysVeg: DayMenu[]
 }
 
 export interface Menu {
-  name: string;
-  startDate: Date;
-  endDate: Date;
-  isActive: boolean;
+  name: string
+  startDate: Date
+  endDate: Date
+  isActive: boolean
 }
