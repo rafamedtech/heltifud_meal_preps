d4cbc6784342597e716f9ac547260dc5569bdfc1
Rafael Amed Valenzuela Gonz[alez <rafamed.tech@gmail.com>
2026-03-25 19:26:52 -0700

    fixed admin pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,7 +12,7 @@ export default defineNuxtConfig({
 
   vite: {
     optimizeDeps: {
-      include: ["zod", "@vue/devtools-core", "@vue/devtools-kit"]
+      include: ["zod", "@vue/devtools-core", "@vue/devtools-kit", "@internationalized/date"]
     }
   },
 
